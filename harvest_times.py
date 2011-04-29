68a57771daed4e2db7971df1003e25b639b71d5f
Eric Buckley <eric.buckley@gmail.com>
2011-04-29 17:28:29 -0400

    finally i think we have a working version, or at least a version that will run on Google App Engine :)

--- harvest_times.py
+++ harvest_times.py
@@ -4,39 +4,52 @@ try:
     import json
 except:
     from django.utils import simplejson as json
+import logging
 import re
-import urllib2
 from xml.etree import ElementTree
 
-import bottle
-from bottle import Bottle, request
+from google.appengine.api import urlfetch
 
-app = Bottle()
+import bottle
+from bottle import request, get, post, error, abort
 
 TIME_re = re.compile(r'.+{t:(\d+)}.*')
 
-@app.error(403)
+def dbg():
+    """ Enter pdb in App Engine
+
+    Renable system streams for it.
+    """
+    import pdb
+    import sys
+    pdb.Pdb(stdin=getattr(sys,'__stdin__'),stdout=getattr(sys,'__stderr__')).set_trace(sys._getframe().f_back)
+
+@error(403)
 def error403(code):
     return 'No auth, No way!'
 
 def auth_required(func):
     def wrapper(*args, **kwargs):
         if not request.auth:
-            return app.abort(403)
+            return abort(403)
         return func(*args, **kwargs)
     return wrapper
 
-@app.get('/')
+@get('/')
 def index():
     return 'Looking for something?'
 
-@app.post('/:domain/:project/:task/')
+@post('/:domain/:project/:task/')
 @auth_required
 def harvest_times(domain, project, task):
     username, password = request.auth
     project_id = _find_project_id(domain, project, username, password)
     task_id = _find_task_id(domain, task, username, password)
-    data = json.loads(request.body.read())
+    try:
+        data = json.loads(request.POST['payload'])
+    except Exception, e:
+        logging.error(request.POST['payload'])
+        logging.fatal('Payload Exception: %s' % e)
     count = 0
     time = 0
     for commit in data['commits']:
@@ -59,17 +72,20 @@ def _find_task_id(domain, task, username, password):
     return _find_id_by_name(response, task)
 
 def _find_id_by_name(xml, name):
-    tree = ElementTree.fromstring(xml)
+    tree = ElementTree.fromstring(xml.content)
     for project in tree.getchildren():
         if project.findtext('name') == name:
             return int(project.findtext('id'))
     return None
 
 def _process_commit(commit, username, project_id, task_id):
+    logging.debug('username: %s' % username)
+    logging.debug('author: %s' % commit['author']['email'])
     if commit['author']['email'] != username:
         # the commit came from someone else, ignore
         return None, None
     message = commit['message']
+    logging.debug('message: %s' % message)
     match = TIME_re.match(message)
     if not match:
         # the commit doesn't contain time tracking, ignore
@@ -88,18 +104,24 @@ def _process_commit(commit, username, project_id, task_id):
             <spent_at type="date">%s</spent_at>
         </request>
     """ % (message, hours, project_id, task_id, date)
-    return xml, hours
+    logging.debug('xml: %s' % xml)
+    return str(xml.replace('\n', ' ')), hours
 
 def _send_to_harvest(domain, path, username, password, data=None):
     """POST the given data message to the harvest API, if no data is provided GET"""
     url = 'https://%s.harvestapp.com/%s' % (domain, path)
     headers = {'Content-type': 'application/xml', 'Accept': 'application/xml', 
         'Authorization': 'Basic %s' % base64.b64encode('%s:%s' % (username, password))}
-    request = urllib2.Request(url, data, headers)
-    return urllib2.urlopen(request).read()
+    logging.debug(url)
+    logging.debug(data)
+    logging.debug(headers)
+    if data:
+        return urlfetch.fetch(url=url, payload=data, method=urlfetch.POST, headers=headers)
+    else:
+        return urlfetch.fetch(url=url, headers=headers)
 
 def main():
-    bottle.run(app, server='gae')
+    bottle.run(server='gae')
 
 if __name__ == '__main__':
     main()
